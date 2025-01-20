6789202dccecc169b3d9386b14f8f3bd3eaf4763
santer545 <obram22@gmail.com>
2025-01-20 19:55:07 +0200

    example of http code and status

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  fetchRequest(@Req() req: Request, @Res() res) {
+    const { id } = req.params;
+    const queryParam = req.query;
+    const userAgent = req.headers['user-agent'];
+
+    return res.send(`<script>
+      console.log('id: ${id}');
+      console.log('queryParam: ${JSON.stringify(queryParam)}');
+      console.log('userAgent: ${userAgent}');</script>`);
   }
 }
