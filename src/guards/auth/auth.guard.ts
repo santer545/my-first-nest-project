ff1fc36b7b7e7ff5278cf60fd3f23bfa28139c6b
santer545 <obram22@gmail.com>
2025-01-22 15:04:20 +0200

    use guards

--- src/guards/auth/auth.guard.ts
+++ src/guards/auth/auth.guard.ts
@@ -1,11 +1,27 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { UserService } from 'src/services/user/user.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private userService: UserService) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const apiKey = request.headers['api_key'];
+
+    const user = this.userService.getUser(apiKey);
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid API key');
+    }
+    request.user = user;
     return true;
   }
 }
